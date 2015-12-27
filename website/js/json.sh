b6b1421fc9b234bd05b6978da3d7004bae856425
nsreehari <nsreehari@outlook.com>
2015-12-27 23:20:11 +0530

    script for generating static db data

--- website/js/json.sh
+++ website/js/json.sh
@@ -1,12 +1,13 @@
 
-curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.vneeds?cols=name,project,mobile,work,fromdate,todate,days,skills,numvolunteers > vneeds.json
+echo > p2index.js
+echo "localjsons=1" >> p2index.js
 
-curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.buses?cols=name,starttime,arrivaltime,droplocation,directions > buses.json
+echo 'vneeds='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.vneeds?cols=name,project,mobile,work,fromdate,todate,days,skills,numvolunteers`''';' >> p2index.js
 
-curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.trains?cols=name,fromstation,fromtime,dropstation,droptime,directions > trains.json
+echo 'buses='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.buses?cols=name,starttime,arrivaltime,droplocation,directions`''';' >> p2index.js
+echo 'trains='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.trains?cols=name,fromstation,fromtime,dropstation,droptime,directions`''';' >> p2index.js
 
-curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.pocs > pocs.json
+echo 'hotels='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.hotels?cols=name,address,distance,hotelcontact,volunteercontact,directions`''';' >> p2index.js
+echo 'pocs='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.pocs`''';' >> p2index.js
 
-curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.hotels?cols=name,address,distance,hotelcontact,volunteercontact,directions > hotels.json
-
-
+cat p2index.js.src >> p2index.js
