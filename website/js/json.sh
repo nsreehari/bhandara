770dbb6c0b87f4c84b06336a9cb0099aeb7d1104
nsreehari <nsreehari@outlook.com>
2015-12-29 12:46:47 +0530

    moved dynamic data into a seperate file

--- website/js/json.sh
+++ website/js/json.sh
@@ -1,16 +1,15 @@
 
-echo > p2index.js
-echo "localjsons=1" >> p2index.js
+echo > p1index.js
+echo "localjsons=1" >> p1index.js
 
-echo 'vneeds='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.vneeds?cols=name,project,mobile,work,fromdate,todate,days,skills,numvolunteers`''';' >> p2index.js
+echo 'vneeds='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.vneeds?cols=name,project,mobile,work,fromdate,todate,days,skills,numvolunteers`''';' >> p1index.js
 
-echo 'buses='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.buses?cols=name,starttime,arrivaltime,droplocation,directions`''';' >> p2index.js
-echo 'trains='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.trains?cols=name,fromstation,fromtime,dropstation,droptime,directions`''';' >> p2index.js
+echo 'buses='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.buses?cols=name,starttime,arrivaltime,droplocation,directions`''';' >> p1index.js
+echo 'trains='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.trains?cols=name,fromstation,fromtime,dropstation,droptime,directions`''';' >> p1index.js
 
-echo 'hotels='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.hotels?cols=name,address,distance,hotelcontact,volunteercontact,directions`''';' >> p2index.js
-echo 'pocs='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.pocs?cols=name,phone,department,email,center`''';' >> p2index.js
+echo 'hotels='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.hotels?cols=name,address,distance,hotelcontact,volunteercontact,directions`''';' >> p1index.js
+echo 'pocs='''`curl http://bitnami-odoo-d47c.cloudapp.net:8068/bitnami/bhandara.pocs?cols=name,phone,department,email,center`''';' >> p1index.js
 
-cat p2index.js.src >> p2index.js
 
 
 
